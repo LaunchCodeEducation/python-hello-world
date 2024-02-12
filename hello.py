953062061549512c483f3fd12fabb95e3ee1b537
John Woolbright <jwoolbright23@gmail.com>
2024-02-12 14:15:46 -0600

    python version of hello world!

---
+++ hello.py
@@ -0,0 +1,19 @@
+
+# Welcome, new coder.
+# Enter your code on line 7 to print 'Hello, World!'
+# to the console (the panel on the right)
+
+print("Hello");
+
+# Next, PLAY!
+# Modify your code on line 7 to try to accomplish the tasks listed below:
+
+#   1. Change the message that is printed.
+#   2. Figure out what the parentheses do. Will the code work without them?
+#   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+#   4. Print a number. (Bonus: Print two numbers added together).
+#   5. Print multiple messages one after the other.
+#   6. Print two messages on the same line.
+#   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+#   8. Other. You choose!
+#
